0846758cef83b8286aaf290a40ce411ca32173a2
ttrottier68-coder <ttrottier68@gmail.com>
2026-04-24 14:52:21 -0700

    Refactor variable declarations in store.ts

--- Remindrr/src/lib/store.ts
+++ Remindrr/src/lib/store.ts
@@ -4,7 +4,7 @@ export type { UserSettings };
 
 const SETTINGS_KEY = 'remindrr_settings';
 const INVOICES_KEY = 'remindrr_invoices';
-const CLIENTS_KEY  = 'remindrr_clients';
-const DATA_SERVER  = 'https://remindrr.onrender.com';
+const CLIENTS_KEY = 'remindrr_clients';
+const DATA_SERVER = 'https://remindrr.onrender.com';
 
-function safe
+function safeGet
