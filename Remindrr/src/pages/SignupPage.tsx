d1756b66d0f39203e8730c09264c5cd802c4e591
ttrottier68-coder <ttrottier68@gmail.com>
2026-04-24 21:13:04 -0700

    Fix import path for saveSettings function

--- Remindrr/src/pages/SignupPage.tsx
+++ Remindrr/src/pages/SignupPage.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import AuthLayout from '../components/AuthLayout';
 import { register } from '../lib/auth';
-import { saveSettings } from '../lib/data-store
+import { saveSettings } from from '../lib/reminder-data';
 
 export default function SignupPage() {
   const [searchParams] = useSearchParams();
