a6434611e66be122f18ac63c43ae657bc395e442
ttrottier68-coder <ttrottier68@gmail.com>
2026-04-24 21:30:47 -0700

    Update import path for reminder-data module

--- Remindrr/src/pages/EditInvoicePage.tsx
+++ Remindrr/src/pages/EditInvoicePage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { getInvoices, getClients, saveInvoice } from '../lib/store';
+import { getInvoices, getClients, saveInvoice } from '../lib/reminder-data'
 import type { Invoice } from '../types';
 
 export default function EditInvoicePage() {
